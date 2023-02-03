27b84507730f362fde45d6b8424ba9536682a5c3
Vu Tien Loi <loi.vt13104@sinhvien.hoasen.edu.vn>
2023-02-03 14:42:36 +0700

    finish

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+let addN = document.getElementById("add-N");
+let inputN = document.getElementById("input-N");
+let resultX = document.getElementById("result");
+function showresultX()
+{
+   let resultItem = f(inputN.value)
+   resultX.innerHTML=resultItem;
+}
+function f(N)
+{
+   if ( N==1) {
+       return 1;
+   }
+   if (N==2)
+   {
+    return 3;
+   }
+   if (N==3)
+   {
+    return 5;
+   }
+   return f(N-1)+ (2*f(N-2) )
+}
+addN.addEventListener("click",showresultX)
